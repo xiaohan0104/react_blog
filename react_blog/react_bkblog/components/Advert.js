ef94e17cdc122c859fb7360cb356a77e57796a35
xiaohan0104 <1940353219@qq.com>
2020-10-21 22:25:24 +0800

    OK

---
+++ react_blog/react_bkblog/components/Advert.js
@@ -0,0 +1,14 @@
+import '../static/style/components/advert.css'
+
+
+const Advert = ()=>{
+        return(
+            <div className='ad-div comm-box'>
+                <div> <img width='100%' src='http://newimg.jspang.com/kaikeba222.png'></img></div>
+                <div> <img width='100%' src='http://newimg.jspang.com/kaikeba222.png'></img></div>
+                <div> <img width='100%' src='http://newimg.jspang.com/kaikeba222.png'></img></div>
+                <div> <img width='100%' src='http://newimg.jspang.com/kaikeba222.png'></img></div>
+            </div>
+        )
+}
+export default Advert
